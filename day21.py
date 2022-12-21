e5395ba1c7d7656da60aa5b140be67de23d97291
jhereth <7327644+jhereth@users.noreply.github.com>
2022-12-21 06:37:54 +0100

    Day 21 - Part 1

--- day21.py
+++ day21.py
@@ -23,29 +23,84 @@ def read_input(s):
 def pw(a, b, op=op.and_):
     return v([op(*_) for _ in zip(a, b)])
 
-DEBUG = 500
+DEBUG = 0
 
 def d(*s, level=0):
     if level>=DEBUG:
         print(*s)
 
 
-test_input = """""".splitlines()
+test_input = """root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+""".splitlines()
 
-data = test_input
 
 def parse_input(data):
-    result = []
+    result = {}
+    for i, l in enumerate(data):
+        if l:
+            d(f"{i:4d}: line {l}")
+            s = l.split(": ")
+            result[s[0]] = s[1]
     return result
 
 
-
+def solve(yells, name, start):
+    line = yells[name]
+    d(f"processing {line}")
+    try:
+        result = int(line)
+        return result
+    except:
+        pass
+    for operation in ["+", "-", "*", "/"]:
+        if operation in line: break
+    d(f"Found {operation} in {line}")
+    split = line.split(" " + operation + " ")
+    d(f"Looking for lhs rhs in {split}")
+    lhs, rhs = split[0], split[1]
+    if lhs == "humn":
+        r = solve(yells, rhs, None)
+        raise Exception(f"Human found staring from {start} ({line} - {r})")
+    if rhs == "humn":
+        r = solve(yells, lhs, None)
+        raise Exception(f"Human found staring from {start} ({line} {r})")
+    lr = solve(yells, lhs, start)
+    rr = solve(yells, rhs, start)
+    result = eval(str(lr) + operation + str(rr))
+    return result
+        
 # assert ==
 
-# data = read_input("20_input.txt")
+
 
 if __name__ == "__main__":
-    # parsed = parse_input(data)
-    # pprint(parsed)
-    print("hello")
+    data = test_input
+    data = read_input("21_input.txt")
+    parsed = parse_input(data)
+    # print(solve(parsed, "humn") )
+    # print(solve(parsed, "ptdq") )
+    # print(solve(parsed, "root") )  # 364367103397416
+    
+    # part 2
+    # root: wvbw + czwc
+    # print(solve(parsed, "wvbw", "wvbw"))  # Human found staring from wvbw (humn - qflr - 594.0)
+    print(solve(parsed, "czwc", "czwc")) # 594
+    print("^^ czwc")
+    print(solve(parsed, "qflr", "qflr"))
+    print("^^ qflr")
+
 
