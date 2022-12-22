d50a9916eae0f272cf701c8147b2ab42725df64e
jhereth <7327644+jhereth@users.noreply.github.com>
2022-12-22 06:00:08 +0100

    Day 21 - done

--- day21.py
+++ day21.py
@@ -23,7 +23,7 @@ def read_input(s):
 def pw(a, b, op=op.and_):
     return v([op(*_) for _ in zip(a, b)])
 
-DEBUG = 0
+DEBUG = 20
 
 def d(*s, level=0):
     if level>=DEBUG:
@@ -52,7 +52,7 @@ def parse_input(data):
     result = {}
     for i, l in enumerate(data):
         if l:
-            d(f"{i:4d}: line {l}")
+            d(f"{i:4d}: line {l}", level=10)
             s = l.split(": ")
             result[s[0]] = s[1]
     return result
@@ -60,10 +60,10 @@ def parse_input(data):
 
 def solve(yells, name, start):
     line = yells[name]
-    d(f"processing {line}")
+    d(f"processing {line=}")
     try:
         result = int(line)
-        return result
+        return result, True
     except:
         pass
     for operation in ["+", "-", "*", "/"]:
@@ -73,34 +73,134 @@ def solve(yells, name, start):
     d(f"Looking for lhs rhs in {split}")
     lhs, rhs = split[0], split[1]
     if lhs == "humn":
-        r = solve(yells, rhs, None)
-        raise Exception(f"Human found staring from {start} ({line} - {r})")
+        r, _ = solve(yells, rhs, None)
+        return r, False
     if rhs == "humn":
-        r = solve(yells, lhs, None)
-        raise Exception(f"Human found staring from {start} ({line} {r})")
-    lr = solve(yells, lhs, start)
-    rr = solve(yells, rhs, start)
+        r, _ = solve(yells, lhs, None)
+        return r, False 
+    # print(f"Trying to solve {line}")
+    lr, lhuman = solve(yells, lhs, start)
+    rr, rhuman = solve(yells, rhs, start)
+    if lhuman or rhuman:
+        return (lr, lhuman, rr, rhuman), False
     result = eval(str(lr) + operation + str(rr))
-    return result
+    return result, True
         
 # assert ==
-
-
+from fractions import Fraction
+from decimal import Decimal
+# @lru_cache
+def reduce(yells, name, final=None, level=0):
+    line = yells[name]
+    # if final:
+    #     if abs(final - int(final)) > 0.1:
+            # raise Exception(f"Deviation in final: {name=} - {final=} - {line=}")
+    ops = {
+        "+":op.add,
+        "-": op.sub,
+        "*": op.mul,
+        "/": op.truediv,
+    }
+    opp = {
+        "+": op.sub,
+        "-": op.add,
+        "*": op.truediv,
+        "/": op.mul, 
+    }
+    d(f"reducing {name=} || {line=} ||  {final=} || {level=}", level=100)
+    if name == "hmn":
+        raise Exception("WTF4")
+    try:
+        # result = Fraction(int(line), 1)
+        result = Decimal(int(line))
+        d(f"returning {result} for || {name=} || {line=} ||  {final=} || {level=}", level=100)
+        return result, False 
+    except:
+        pass
+    for operation in ["+", "-", "*", "/"]:
+        if operation in line: break
+    else:
+        raise Exception(f"No known operation in {line=} ({name=})")
+    d(f"found operation {operation} in {line}", level=-10)
+    split = line.split(" " + operation + " ")
+    lhs, rhs = split[0], split[1]
+    if lhs == "humn":
+        if final is None:
+            d(f"returning 'Human found' for || {name}: {line} || {lhs=} || {rhs=} || {line=} {final=} || {level=}", level=100)
+            return None, True
+        rr, rhuman = reduce(yells, rhs, None, level=level+1)
+        d(f"left Human Found almost a Solution {final=} - || {line=} || {operation=} - {opp[operation]=} - {rr=} - {rhuman=}", level=100)
+        equation = str(final) + str(opp[operation]) + str(rr)
+        result = opp[operation](final, rr)
+        # if abs(result - int(result)) > 0.1:
+        #     raise Exception(f"Deviation: {result=} - {equation=} - {name=} - {final=} - {rr=}")
+        if name == "root":
+            d(f"Found root Solution {result=} || {final=} - {operation=} - {opp[operation]=} - {rr=} - {rhuman=}", level=100)
+            raise Exception(f"{line} with rr={rr}")
+        d(f"Found final Solution {result=} || {final=} - {operation=} - {opp[operation]=} - {rr=} - {rhuman=}", level=100)
+        raise Exception(f"The human variable is {result} ({line=} - {name=} - {final=} - {rr=})") 
+    if rhs == "humn":
+        if final is None:
+            d(f"returning 'Human found' for || {name:} {line} ||  {lhs=} | {rhs=} | {final=} || {level=}", level=100)
+            return None, True
+        lr, _ = reduce(yells, lhs, final, level=level+1)
+        d(f"right Human Found almost a Solution {final=} - || {line=} || {operation=} - {ops[operation]=} - {lr=} - {lhuman=}", level=100)
+        result = ops[operation](lr, final)
+        # if abs(result - int(result)) > 0.1:
+        #     raise Exception(f"Deviation: {result=} - {equation=} - {name=} - {final=} - {lr=}")
+        raise Exception(f"The human variable is {result=} ({line=} - {name=} - {final=} - {lr=})") 
+    d(f"No human found in {final}=={line} (yet).", level=-5)
+    lr, lhuman = reduce(yells, lhs, None, level=level+1)
+    rr, rhuman = reduce(yells, rhs, None, level=level+1)
+    if lhuman and rhuman:
+        raise Exception("WTF")
+    if lhuman:
+        side = lhs
+        val_other_side = rr
+        # new_final = rr
+        equation = str(final) + str(opp[operation]) + str(rr)
+        if final is not None:
+            new_final = opp[operation](final, rr)
+            d(f"{name} left Human: Calculated {new_final=} from {line=} and {final=} - {rr=}", level=30)
+    if rhuman:
+        side = rhs
+        val_other_side = lr
+        if final is not None:
+            if operation in ["-", "/"]:
+                new_final = ops[operation](lr ,final)
+            else:
+                new_final = opp[operation](final, lr)
+            d(f"{name} right Human: Calculated {new_final=} from {line=} and {final=} - {lr=}", level=30)
+    if lhuman or rhuman:
+        if final is None:
+            if name != "root":
+                d(f"returning summary 'Human found' for || {name}: {line} || {lhuman=} || {rhuman=} || {line=} ||  {final=} || {level=}", level=100)
+                return None, True
+            d(f"Descending to solve: {name=} | {line=} | {val_other_side=}")
+            val, human = reduce(yells, side, val_other_side, level=level+100_000)
+            raise Exception(f"WTF2 - {val} - {human}")
+        d(f"Trying to solve with {new_final=} | {name=} | {line=}")
+        val, human = reduce(yells, side, new_final, level=level+1)
+        raise Exception("WTF3")
+    # result = eval(str(lr) + operation + str(rr))
+    result = ops[operation](lr, rr)
+    # if abs(result - int(result)) > 0.1:
+        # raise Exception(f"Deviation: {result=} - {equation=} - {name=} - {final=} - {lr=}")
+    d(f"returning {result} for || {name=} || {line=} ||  {final=} || {level=}", level=100)
+    return result, False
 
 if __name__ == "__main__":
+    data="""root: this / number
+number: 7
+this: foo / humn
+foo: 7""".splitlines()
+    # pprint(data)
     data = test_input
-    data = read_input("21_input.txt")
+    # data = read_input("21_input.txt")
     parsed = parse_input(data)
+    # pprint(parsed)
     # print(solve(parsed, "humn") )
     # print(solve(parsed, "ptdq") )
     # print(solve(parsed, "root") )  # 364367103397416
+    print(reduce(parsed, "root", None))  # 3782852515583
     
-    # part 2
-    # root: wvbw + czwc
-    # print(solve(parsed, "wvbw", "wvbw"))  # Human found staring from wvbw (humn - qflr - 594.0)
-    print(solve(parsed, "czwc", "czwc")) # 594
-    print("^^ czwc")
-    print(solve(parsed, "qflr", "qflr"))
-    print("^^ qflr")
-
-
